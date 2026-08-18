5730a300c5be5c2c47ab24aa819c6121c58f4e1b
Andrei Stepanov <2x2z5@mail.ru>
2026-08-19 01:48:16 +0400

    Major refactoring of the project

--- Test/test_version.cpp
+++ Test/test_version.cpp
@@ -1,10 +1,10 @@
-#define BOOST_TEST_MODULE test_version
+#define BOOST_TEST_MODULE helloworld_test
 
-#include "lib.h"
+#include "Lib/lib.h"
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(helloworld_test)
 
 BOOST_AUTO_TEST_CASE(test_valid_version) {
 	BOOST_CHECK(version() > 0);
